68dc13a3b05866d3cc6cd23c2541dd420cbcac84
abdurrashidkhan <rashidkhan.destop@gmail.com>
2024-09-13 11:37:38 +0600

    anather section added success

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "graph.facebook.com",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
+  // webpack: (config, { isServer }) => {
+  //   // Add your custom webpack configurations if needed
+  //   return config;
+  // },
+};
 
 export default nextConfig;
