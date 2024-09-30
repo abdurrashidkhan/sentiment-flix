cea9084ca6c5467ba23c8eb33de11293d13853da
abdurrashidkhan <rashidkhan.destop@gmail.com>
2024-09-30 09:57:23 +0600

    error haandel

--- src/app/api/update/banner/[...data]/route.js
+++ src/app/api/update/banner/[...data]/route.js
@@ -1,95 +1,95 @@
-import connectMongodb from "@/lib/mongodb";
-import banner from "@/models/bannerSchema";
-import { ObjectId } from "mongodb";
-import { NextResponse } from "next/server";
+// import connectMongodb from "@/lib/mongodb";
+// import banner from "@/models/bannerSchema";
+// import { ObjectId } from "mongodb";
+// import { NextResponse } from "next/server";
 
-export async function PUT(request, { params }) {
-  const category = params?.data[0];
-  const id = params?.data[1];
-  await connectMongodb();
-  const filter = { _id: new ObjectId(id) };
-  const options = { upsert: false };
-  if (category === "movies") {
-    const {
-      title,
-      freeOrPaid,
-      catagories,
-      displaySection,
-      season,
-      episode,
-      contentSummary,
-      date,
-      smImage,
-      lgImage,
-      description,
-      video,
-    } = await request.json();
-    const updateDoc = {
-      title,
-      freeOrPaid,
-      catagories,
-      displaySection,
-      season,
-      episode,
-      contentSummary,
-      date,
-      smImage,
-      lgImage,
-      description,
-      video,
-    };
+// export async function PUT(request, { params }) {
+//   const category = params?.data[0];
+//   const id = params?.data[1];
+//   await connectMongodb();
+//   const filter = { _id: new ObjectId(id) };
+//   const options = { upsert: false };
+//   if (category === "movies") {
+//     const {
+//       title,
+//       freeOrPaid,
+//       catagories,
+//       displaySection,
+//       season,
+//       episode,
+//       contentSummary,
+//       date,
+//       smImage,
+//       lgImage,
+//       description,
+//       video,
+//     } = await request.json();
+//     const updateDoc = {
+//       title,
+//       freeOrPaid,
+//       catagories,
+//       displaySection,
+//       season,
+//       episode,
+//       contentSummary,
+//       date,
+//       smImage,
+//       lgImage,
+//       description,
+//       video,
+//     };
 
-    const result = await banner.findOneAndUpdate(
-      filter,
-      { $set: updateDoc },
-      options
-    );
-    return NextResponse.json(
-      { message: "update data success", status: true },
-      { status: 201 }
-    );
-  } else {
-    const {
-      title,
-      seriesName,
-      freeOrPaid,
-      displaySection,
-      catagories,
-      season,
-      episode,
-      contentSummary,
-      description,
-      date,
-      lgImage,
-      smImage,
-      video,
-    } = await request.json();
-    const filter = { _id: new ObjectId(id) };
-    const options = { upsert: false };
-    const updateDoc = {
-      title,
-      seriesName,
-      freeOrPaid,
-      displaySection,
-      catagories,
-      season,
-      episode,
-      contentSummary,
-      description,
-      date,
-      lgImage,
-      smImage,
-      video,
-    };
+//     const result = await banner.findOneAndUpdate(
+//       filter,
+//       { $set: updateDoc },
+//       options
+//     );
+//     return NextResponse.json(
+//       { message: "update data success", status: true },
+//       { status: 201 }
+//     );
+//   } else {
+//     const {
+//       title,
+//       seriesName,
+//       freeOrPaid,
+//       displaySection,
+//       catagories,
+//       season,
+//       episode,
+//       contentSummary,
+//       description,
+//       date,
+//       lgImage,
+//       smImage,
+//       video,
+//     } = await request.json();
+//     const filter = { _id: new ObjectId(id) };
+//     const options = { upsert: false };
+//     const updateDoc = {
+//       title,
+//       seriesName,
+//       freeOrPaid,
+//       displaySection,
+//       catagories,
+//       season,
+//       episode,
+//       contentSummary,
+//       description,
+//       date,
+//       lgImage,
+//       smImage,
+//       video,
+//     };
 
-    const result = await banner.findOneAndUpdate(
-      filter,
-      { $set: updateDoc },
-      options
-    );
-    return NextResponse.json(
-      { message: "update data success", status: true },
-      { status: 201 }
-    );
-  }
-}
+//     const result = await banner.findOneAndUpdate(
+//       filter,
+//       { $set: updateDoc },
+//       options
+//     );
+//     return NextResponse.json(
+//       { message: "update data success", status: true },
+//       { status: 201 }
+//     );
+//   }
+// }
